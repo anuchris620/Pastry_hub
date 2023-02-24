edf90db8b04c1488440940034ca3d85374019f67
Anu christin <anuchris620@gmail.com>
2023-02-24 10:43:03 +0530

    session completed(4 cake product and made interative)

--- product/views.py
+++ product/views.py
@@ -6,9 +6,32 @@ def pro(request):
 
 def prodetails(request):
     ids=request.GET['id']
-    data=CakePr.objects.get(id=ids)
-    #return render(request,'single_post.html',{"prodetails":data})
-    return render(request,'single-product.html',{"prodetails":data})
+    data=CakePr.objects.get(id=ids) #id is database id compare with ids to take the product
+    if "recent_views" in request.session:           #another area in cookies
+        if ids in request.session["recent_views"]:
+            request.session["recent_views"].remove(ids)
+
+        
+        if len(request.session["recent_views"])>4:
+            request.session["recent_views"].pop()
+        print('hiiiii', request.session["recent_views"])
+        recent=[]
+        for i in request.session["recent_views"]:
+            recent.append(CakePr.objects.get(id=i))
+        request.session["recent_views"].insert(0,ids)
+        request.session.modified=True
+        
+        return render(request,'single-product.html',{"prodetails":data,"recent":recent})
+
+        #recent=CakePr.objects.filter(id__in=request.session["recent_views"]) #id__in to take multiple values
+        #print("hello", recent) 
+    else:
+        request.session["recent_views"]=[ids]  #is the list method for creating empty list
+        request.session.modified=True
+
+        #return render(request,'single_post.html',{"prodetails":data})
+        return render(request,'single-product.html',{"prodetails":data})
+
 
 def commentarea(request):
     cmd=request.GET['cmt']
